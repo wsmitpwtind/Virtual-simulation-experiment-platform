93022d79cf04f5bde9cbf8e8cf39617871fe4d4b
flash123456789 <918127890@qq.com>
2020-11-02 22:23:35 +0800

    11/2

---
+++ Assets/scripts/Create_trigonum.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Create_trigonum : MonoBehaviour
+{
+    public GameObject My_trigonum;
+    Vector3 position = new Vector3(0, 1.94f, -3f);
+    Quaternion rotation = Quaternion.Euler(new Vector3(-90f, 0, 0));
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Trigonum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Trigonum()
+    {
+        GameObject trigonum = GameObject.Instantiate(My_trigonum, position, rotation) as GameObject;
+    }
+}
---
+++ Assets/scripts/Create_vernier.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Create_vernier : MonoBehaviour
+{
+    public GameObject My_vernier;
+    Vector3 position = new Vector3(0, 2f, -2.5f);
+    Quaternion rotation = Quaternion.Euler(new Vector3(-90f, 0, 0));
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Vernier);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Vernier()
+    {
+        GameObject trigonum = GameObject.Instantiate(My_vernier, position, rotation) as GameObject;
+    }
+}
