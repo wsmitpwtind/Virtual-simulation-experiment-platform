6e11f53c54a394146ae4cb2e376069acc0e4ce19
wojiaocbj@163.com <wojiaocbj@163.com>
2020-12-11 20:27:29 +0800

    测量书demo to be continued

---
+++ Assets/Cbj/MoveRuler3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRuler3 : MonoBehaviour {
+    // Start is called before the first frame update
+    protected GameObject book, ruler1, ruler2;
+    protected Camera cam;
+    private void Start() {
+        book = GameObject.Find("book_0001b");
+        ruler1 = GameObject.Find("rulerRescaled");
+        ruler2 = GameObject.Find("VernierRescaled");
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Z)) {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.X)) {
+
+        }
+        else if(Input.GetKeyDown(KeyCode.C)) {
+
+        }
+    }
+    private void MeasureX() {
+
+    }
+    private void MeasureY() {
+
+    }
+    private void MeasureZ() {
+
+    }
+    private void OnCollisionEnter(Collision collision) {
+
+    }
+}
