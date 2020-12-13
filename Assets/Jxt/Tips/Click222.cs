52053167693ee3f834420fbfc1d609140aafd523
flash123456789 <918127890@qq.com>
2020-12-13 23:12:16 +0800

    123

--- Assets/Jxt/Tips/Click222.cs
+++ Assets/Jxt/Tips/Click222.cs
@@ -5,12 +5,15 @@ using UnityEngine.UI;
 
 public class Click222 : MonoBehaviour
 {
+    private GameObject canvas;
     bool shown = false;
     TipsController tipsController = null;
 
     // Start is called before the first frame update
     void Start()
     {
+        canvas = GameObject.Find("Canvas");
+
         tipsController = GameObject.Find("Tips").GetComponent<TipsController>();
         gameObject.GetComponent<Button>().onClick.AddListener(switchTips);
     }
@@ -28,12 +31,16 @@ public class Click222 : MonoBehaviour
             // tipsController.ShowTips("嘿嘿，这道题选什么你猜啊。\n猜不到吧233\nhhhhh");
             tipsController.HideTips();
             shown = false;
+
+            canvas.transform.Find("wwl").gameObject.SetActive(false);//张峻凡
         }
         else
         {
             tipsController.ShowTips("嘿嘿，这道题选什么你猜啊。\n猜不到吧\nhhhhh");
             // tipsController.ShowTips("嘿嘿，这道题选什么你猜啊。\n猜不到吧2222\nhhhhh");
             shown = true;
+
+            canvas.transform.Find("wwl").gameObject.SetActive(true);//张峻凡
         }
     }
 }
