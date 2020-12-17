06f05c5143c0481b17ca025d03dd9dd18b9d1750
Extra2001 <jingxutian@outlook.com>
2020-12-17 20:00:18 +0800

    Add JSON Local Storage

---
+++ Assets/Jxt/testIO.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testIO : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(hh);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void hh()
+    {
+        // GameObject obj =  IOHelper.AddPrefab();
+
+        // GameObject.Instantiate<GameObject>(obj);
+
+        Storage.SetStorage("button", new Model123 (){
+            name = "WO",
+            age = 122
+        });
+
+        Debug.Log(Storage.GetStorage<Model123>("button").age);
+    }
+    class Model123
+    {
+        public  string name { get; set; }
+        public int age { get; set; }
+    }
+}
