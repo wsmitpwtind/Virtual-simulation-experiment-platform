9c48b9eb5cc24fafd5b7b89b7d1d24c150cacbe5
wsmitpwtind <wsmitpwtind@163.com>
2020-12-23 10:31:09 +0800

    qwq

---
+++ Assets/scripts/Show_place.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Show_place : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Camera mCamera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            GameObject RuC = GameObject.Find("Camera");
+            this.transform.position = RuC.transform.position;
+            this.transform.rotation = RuC.transform.rotation;
+            mCamera = gameObject.GetComponent<Camera>();
+            mCamera.fieldOfView = 30.0f;
+        }
+    }
+}
