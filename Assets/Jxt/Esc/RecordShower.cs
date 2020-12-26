38d1f5a8defaa116c56e6a95ff37bc544683009d
Extra2001 <jingxutian@outlook.com>
2020-12-26 08:53:45 +0800

    add continue

--- Assets/Jxt/Esc/RecordShower.cs
+++ Assets/Jxt/Esc/RecordShower.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class RecordShower : MonoBehaviour
 {
     public GameObject RecordObj;
+    public GameObject EmptyObj;
+    private GameObject emptyObj;
     private List<RecordScript> showedRecords = new List<RecordScript>();
     void Start()
     {
@@ -16,13 +18,19 @@ public class RecordShower : MonoBehaviour
         foreach (var item in showedRecords)
             Destroy(item.gameObject);
         showedRecords.Clear();
+        if (emptyObj != null)
+            Destroy(emptyObj);
+        emptyObj = null;
         var list = RecordManager.recordInfos;
-        foreach (var item in list)
-        {
-            var recordScript = Instantiate<GameObject>(RecordObj, gameObject.transform).GetComponent<RecordScript>();
-            recordScript.SetRecordInfo(item);
-            showedRecords.Add(recordScript);
-        }
+        if (list.Count == 0)
+            emptyObj = Instantiate<GameObject>(EmptyObj, gameObject.transform);
+        else
+            foreach (var item in list)
+            {
+                var recordScript = Instantiate<GameObject>(RecordObj, gameObject.transform).GetComponent<RecordScript>();
+                recordScript.SetRecordInfo(item);
+                showedRecords.Add(recordScript);
+            }
     }
 
     void OnDestroy()
---
+++ Assets/Jxt/Start/ContinueButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ContinueButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (RecordManager.RecordContains(RecordManager.currentRecordId))
+        {
+            GetComponent<Button>().onClick.AddListener(conti);
+        }
+        else
+        {
+            GetComponent<Image>().color = new Color(50, 50, 50);
+            GetComponent<Button>().interactable = false;
+        }
+    }
+    void conti()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
