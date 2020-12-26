220758fa6d22afc197b775e3e3c30bd1ac29ac82
Extra2001 <jingxutian@outlook.com>
2020-12-26 09:51:59 +0800

    add auto save

---
+++ Assets/Jxt/Record/AutoSaveRecord.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSaveRecord : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RecordManager.SaveRecord(Record.GenRecordInfo(RecordManager.currentRecordId, $"自动保存的 {RecordManager.currentRecordId}"));
+    }
+}
